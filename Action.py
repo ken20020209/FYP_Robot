32dbe420082942e29769ad1a6641285ac9ede121
ken20020209 <ken20020209@gmail.com>
2024-01-30 21:19:00 +0800

    complete action class

---
+++ Action.py
@@ -0,0 +1,47 @@
+#common lib
+import os
+import sys
+import time
+import cv2 as cv
+from cv_bridge import CvBridge
+import numpy
+
+from lib.DOGZILLALib import DOGZILLA
+
+#ros2 lib
+import rclpy
+from rclpy.node import Node
+from std_msgs.msg import String
+from sensor_msgs.msg import Image, CompressedImage
+from std_msgs.msg import Int32;
+
+# print("import lib success")
+
+class Action(Node):
+    def __init__(self,name='Action'):
+        super().__init__(name)
+        self.subAction= self.create_subscription(Int32,'/action',self.action_callback,10)
+        self.dog=DOGZILLA()
+        
+    def action_callback(self,msg):
+        
+        if(msg.data<=0 or msg.data>255):
+            print("out of action range 1-18 , 255") 
+        else:
+            self.dog.action(msg.data)
+
+        
+        
+
+        
+
+def main():
+    rclpy.init()
+    action = Action()
+    rclpy.spin(action)
+    action.destroy_node()
+    rclpy.shutdown()
+
+if __name__ == '__main__':
+    main()
+    
