07bb7a2c6df6e224adee79222fa5ad2fa2d6f75c
ken20020209 <ken20020209@gmail.com>
2024-02-23 20:39:06 +0800

    add start file

---
+++ startController.py
@@ -0,0 +1,19 @@
+import os
+from time import sleep
+from threading import Thread
+from multiprocessing import Process,Manager,Value
+
+
+# os.environ['ROS_DOMAIN_ID'] = '16'
+
+# os.system(f"source /home/ken20020209/fyp/FYP_Server/ROS_Server_WS/install/setup.bash")
+# os.system(f"ros2 launch basic Slam.launch.py ekf_node:=true")
+
+#output my current file directory not pwd path
+print(os.path.dirname(os.path.abspath(__file__)))
+
+# print(os.path.dirname(os.path.abspath(__file__)))
+
+
+
+os.system(f"/bin/bash -c 'source {os.path.dirname(os.path.abspath(__file__))}/install/setup.bash && ros2 launch basic RobotDogController.launch.py'")
