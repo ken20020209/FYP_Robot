e6a1c46717cf145b2520f51800ef24683cc62bc7
ken20020209 <ken20020209@gmail.com>
2024-03-09 12:37:47 +0800

    Fix parameter value assignment in RobotDogConnector.py

--- src/basic/basic/RobotDogConnector.py
+++ src/basic/basic/RobotDogConnector.py
@@ -81,7 +81,7 @@ class RobotDogConnector(Node):
         self.get_logger().info('service available')
         request = RegisterDog.Request()
         request.dog_id = self.name
-        request.type= str(self.get_parameter('type'))
+        request.type= str(self.get_parameter('type').value)
         
         future = self.registerClient.call_async(request)
         rclpy.spin_until_future_complete(self, future)
