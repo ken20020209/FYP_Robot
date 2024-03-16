3f9c6a289055b623d72a8b1b8eea9d8dabbf2213
ken20020209 <ken20020209@gmail.com>
2024-03-16 18:00:46 +0800

    Update startConnector.py and .gitignore

--- startConnector.py
+++ startConnector.py
@@ -3,6 +3,8 @@ from time import sleep
 from threading import Thread
 from multiprocessing import Process,Manager,Value
 
+import json
+
 
 # os.environ['ROS_DOMAIN_ID'] = '16'
 
@@ -14,11 +16,13 @@ print(os.path.dirname(os.path.abspath(__file__)))
 
 # get name for env file
 name = "dog_s2_0"
-with open(f"{os.path.dirname(os.path.abspath(__file__))}/env") as f:
-    name = f.readline()
-    name = name.split(":")[1]
+robot_type = "dog_s2"
+with open(f"{os.path.dirname(os.path.abspath(__file__))}/env.json") as f:
+    data = json.load(f)
+    name = data["name"]
+    robot_type = data["type"]
 # print(os.path.dirname(os.path.abspath(__file__)))
 
 os.system(f" sudo systemctl stop yahboom_oled.service")
 os.system(f"sudo systemctl stop YahboomStart.service")
-os.system(f"/bin/bash -c 'source {os.path.dirname(os.path.abspath(__file__))}/install/setup.bash && ros2 launch basic RobotDogConnector.launch.py name:={name} tpye:=dog_s2'")
+os.system(f"/bin/bash -c 'source {os.path.dirname(os.path.abspath(__file__))}/install/setup.bash && ros2 launch basic RobotDogConnector.launch.py name:={name} tpye:={robot_type}'")
