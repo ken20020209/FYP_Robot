2f1816367055192a990aebb002600101be2db4a8
ken20020209 <94442724+ken20020209@users.noreply.github.com>
2024-03-21 12:02:56 +0800

    Update autoStart.bash
    fix can't start on gnome

--- autoStart.bash
+++ autoStart.bash
@@ -1,11 +1,14 @@
 #!/bin/bsah
 
+sleep 8
 # 切換到您的儲存庫目錄
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 cd $SCRIPT_DIR
 
 # 更新遠端參考
-bash update.bash
+#bash update.bash
 
 # start startConnector python
-python3 startConnector.py
+#python3 startConnector.py
+
+gnome-terminal --bash -c "bash update.bash;python3 startConnector.py;exec bash"
